88dc809bdc521e58ec7aadefd5d7d00825393e22
Phil25 <phil25@protonmail.com>
2017-07-22 16:58:46 +0200

    Update #1

--- bot.js
+++ bot.js
@@ -1,1 +1,34 @@
-console.log('kek');
+try{
+	var config = require('./config');
+}catch(err){
+	console.error("Error parsing config.json:\n" + err.message);
+	return;
+}
+
+var SteamUser	= require('steam-user'),
+	client		= new SteamUser(),
+	SteamTotp	= require('steam-totp');
+
+client.logOn({
+	"accountName": config.username,
+	"password": config.password,
+	"twoFactorCode": SteamTotp.getAuthCode(config.sharedSecret)
+});
+
+client.on('loggedOn', function(details){
+	console.log("Logged into Steam as " + client.steamID.getSteam3RenderedID());
+	client.setPersona(SteamUser.EPersonaState.Online);
+	//client.gamesPlayed(440);
+});
+
+client.on('error', function(e){
+	console.error(e);
+});
+
+/*client.on('webSession', function(sessionID, cookies){
+	console.log("Got web session");
+});
+
+client.on('newItems', function(count){
+	console.log(count + " new items in inventory");
+});*/
