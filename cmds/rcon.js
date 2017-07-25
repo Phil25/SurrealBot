efea2e8963364cf072a0fd6553c75cdcb200a565
Phil25 <phil25@protonmail.com>
2017-07-25 22:49:48 +0200

    Two-way responses with server-bot communication

--- cmds/rcon.js
+++ cmds/rcon.js
@@ -1,23 +1,24 @@
 var	EAccessLevel	= require('../enums/EAccessLevel.js'),
 	ECommandResult	= require('../enums/ECommandResult.js');
 
-module.exports.accessLevel	= EAccessLevel.Admin;
-module.exports.names		= ["say", "s", "sayto"];
-module.exports.description	= "Say something as Sophie.";
+module.exports.accessLevel	= EAccessLevel.HeadAdmin;
+module.exports.names		= ["rcon", "r", "remote", "cmd", "command"];
+module.exports.description	= "Execute an rcon command. (use with caution)";
 
 
 //----------[ Exec ]----------//
 
 module.exports.exec = function(data){
 	if(data.args.length < 3){
-		data.client.chatMessage(data.steamID, "Usage: !say <server> <message>");
+		data.client.chatMessage(data.steamID, "Usage: !rcon <server> <command>");
 		return;
 	}
 	var command = "";
 	for(var i = 2; i < data.args.length; i++)
 		command += data.args[i] + " ";
-	command = command.replace(";", "");
-	if(!data.net.send(data.args[1], "bot_say " + command))
+	if(!data.net.send(data.args[1], command, function(message){
+		data.client.chatMessage(data.steamID, message);
+	}))
 		data.client.chatMessage(data.steamID, "Invalid server specifier or server offline.");
 	return ECommandResult.OK;
 }
